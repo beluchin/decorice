a343a77eb7db6177cacc822b4db23f839c67aab7
Abel Quiros <beluchin@gmail.com>
2015-05-20 08:01:00 -0400

    made Decorate class nested and private to DecoratorModule

--- src/main/java/decorice/DecoratorModule.java
+++ src/main/java/decorice/DecoratorModule.java
@@ -72,6 +72,43 @@ public class DecoratorModule implements com.google.inject.Module {
         }
     }
 
+    private static class Decorated implements DecoratedBy {
+        private final Class<?> value;
+
+        private Decorated(final Class<?> value) {
+            this.value = value;
+        }
+
+        public static DecoratedBy by(final Class<?> class__) {
+            return new Decorated(class__);
+        }
+
+        @Override
+        public Class<? extends Annotation> annotationType() {
+            return DecoratedBy.class;
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (this == obj) return true;
+            if (obj == null || !(obj instanceof DecoratedBy)) {
+                return false;
+            }
+            final DecoratedBy rhs = (DecoratedBy) obj;
+            return value().equals(rhs.value());
+        }
+
+        @Override
+        public int hashCode() {
+            return 127 * "value".hashCode() ^ value.hashCode();
+        }
+
+        @Override
+        public Class<?> value() {
+            return value;
+        }
+    }
+
     private Class targetClass;
     private List<Class> classes;
     private Optional<Function<
--- src/test/java/toremove/OtherAttempts.java
+++ src/test/java/toremove/OtherAttempts.java
@@ -7,8 +7,8 @@ import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.binder.AnnotatedBindingBuilder;
 import com.google.inject.binder.ScopedBindingBuilder;
-import decorice.Decorated;
 import decorice.DecoratedBy;
+import decorice.DecoratorModule;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -96,6 +96,43 @@ public class OtherAttempts {
 
         public static DecoratorBindingModule ex(AbstractModule m) {
             return new DecoratorBindingModule(m);
+        }
+    }
+
+    private static class Decorated implements DecoratedBy {
+        private final Class<?> value;
+
+        private Decorated(final Class<?> value) {
+            this.value = value;
+        }
+
+        public static DecoratedBy by(final Class<?> class__) {
+            return new Decorated(class__);
+        }
+
+        @Override
+        public Class<? extends Annotation> annotationType() {
+            return DecoratedBy.class;
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (this == obj) return true;
+            if (obj == null || !(obj instanceof DecoratedBy)) {
+                return false;
+            }
+            final DecoratedBy rhs = (DecoratedBy) obj;
+            return value().equals(rhs.value());
+        }
+
+        @Override
+        public int hashCode() {
+            return 127 * "value".hashCode() ^ value.hashCode();
+        }
+
+        @Override
+        public Class<?> value() {
+            return value;
         }
     }
 
